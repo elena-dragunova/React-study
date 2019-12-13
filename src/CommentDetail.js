3fcd76bb0518cf4fb8a64bdd506dcd4f2acf6924
elena-dragunova <elenak-8647@gmail.com>
2019-12-13 11:46:42 +0500

    Add ApprovalCard parent component.

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -14,7 +14,9 @@ const CommentDetail = props => {
         <div className="metadata">
           <span className="date">{ props.timeAgo }</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">
+            { props.text }
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentDetail from './CommentDetail'
 import faker from 'faker';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
+
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"
-                     text="Nice blog post!"
-                     image={faker.image.avatar()}
-                     timeAgo="Today at 4:45 PM" />
-      <CommentDetail author="Tom"
-                     text="Awesome!"
-                     image={faker.image.avatar()}
-                     timeAgo="Today at 2:00 PM"/>
-      <CommentDetail author="Jane"
-                     text="Boring..."
-                     image={faker.image.avatar()}
-                     timeAgo="Yesterday at 12:03 PM"/>
+        <ApprovalCard>
+            <CommentDetail author="Sam"
+                           text="Nice blog post!"
+                           image={faker.image.avatar()}
+                           timeAgo="Today at 4:45 PM" />
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail author="Tom"
+                           text="Awesome!"
+                           image={faker.image.avatar()}
+                           timeAgo="Today at 2:00 PM"/>
+        </ApprovalCard>
+
+        <ApprovalCard>
+            <CommentDetail author="Jane"
+                           text="Boring..."
+                           image={faker.image.avatar()}
+                           timeAgo="Yesterday at 12:03 PM"/>
+        </ApprovalCard>
+
     </div>
   );
 };
